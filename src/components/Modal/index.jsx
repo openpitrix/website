4761c7db7306064dcedc9983dd50ab0e37d91f81
dongrui <dannydong@yunify.com>
2019-02-27 15:57:45 +0800

    feat: Add download and mail modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,74 +1,53 @@
 import React from 'react'
-import ReactModal from 'react-modal';
+import ReactModal from 'react-modal'
 import PropTypes from 'prop-types'
-import { omit } from 'lodash';
+import { omit } from 'lodash'
 
-import { Style } from './styled'
+import styles from './index.module.scss'
 
-const customStyles = {
-  content : {
-    position: 'absolute',
-    width: '700px',
-    height: '300px',
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
-};
-
-/*const { content } = ReactModal.defaultStyles
-
-Object.assign(ReactModal.defaultStyles.overlay, customStyles.overlay)
-
-ReactModal.defaultStyles.content = Object.assign(
-  {},
-  omit(content, ['top', 'left', 'right', 'bottom', 'padding']),
-  customStyles.content
-)*/
 export default class Modal extends React.Component {
   static propTypes = {
     children: PropTypes.any,
     closable: PropTypes.bool,
-    title: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
-    visible: PropTypes.bool,
+    isOpen: PropTypes.bool,
     width: PropTypes.number,
+    onClose: PropTypes.func,
   }
 
   static defaultProps = {
-    width: 744,
+    closable: true,
+    isOpen: false,
+  }
+
+  onClose = () => {
+    this.props.onClose && this.props.onClose();
   }
 
   render() {
-    const {
-      title,
-      width,
-      visible,
-      children,
-      onCancel,
-      closable,
-    } = this.props
+    const { width, isOpen, children, closable } = this.props
+
+    const customStyles = {
+      content: {
+        width: width,
+      },
+    }
 
     return (
-      <Style>
       <ReactModal
-        className='modal'
+        ariaHideApp={false}
+        className={styles.modal}
         style={customStyles}
-        isOpen={visible}
-        onRequestClose={onCancel}
-        shouldCloseOnOverlayClick={false}
+        isOpen={isOpen}
+        onRequestClose={this.onClose}
+        shouldCloseOnOverlayClick={true}
       >
-        <div className="header">
-          <div className="title">{title}</div>
-            {closable && (
-              <label className="close">X</label>
-            )}
-          </div>
-        <div className="body">{children}</div>
+        <div className={styles.header}>
+          {closable && (
+            <img onClick={this.onClose} src="/images/modal/close.svg" className={styles.close} />
+          )}
+        </div>
+        <div className={styles.body}>{children}</div>
       </ReactModal>
-      </Style>
     )
   }
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-import Modal from '../../components/Modal'
+import DownloadModal from '../../components/DownloadModal'
+import MailModal from '../../components/MailModal'
 import banners from './banners'
 
 import styles from './index.module.scss'
@@ -10,6 +11,8 @@ import styles from './index.module.scss'
 const mobileWidth = 768
 export default class Home extends Component {
   state = {
+    isDownloadOpen: false,
+    isMailOpen: false,
     isMobile: document.body.clientWidth <= mobileWidth,
   }
 
@@ -25,17 +28,60 @@ export default class Home extends Component {
     }
   }
 
-  renderModal() {
+  openVersionModal = () => {
+    this.setState({
+      isDownloadOpen: true,
+      isMailOpen: false,
+    })
+  }
+
+  submitDownload = () => {
+    this.setState({
+      isDownloadOpen: false,
+      isMailOpen: true,
+    })
+  }
+
+  closeModal = () => {
+    this.setState({
+      isDownloadOpen: false,
+      isMailOpen: false,
+    })
+  }
+
+  renderSceneBanner() {
     return (
-      <Modal visible className="getVersion">
-        <div className="title">获取社区版</div>
-        <div className="description">
-          已经有超过 1000 名用户下载并使用了 OpenPitrix ，如有问题可通过 GitHub
-          或者 Slack 与我们联系
+      <div className={styles.sceneBanner}>
+        <div className={styles.sceneContent}>
+          <div className={styles.title}>应用场景</div>
+          <div className={styles.scene}>
+            <img className={styles.currentImg} src="/images/home/scene-1.svg" />
+            <div className={styles.name}>快速构建行业应用商店 </div>
+            <div className={styles.description}>
+              在以应用为核心的行业云或集团云构建中，基于 OpenPitrix
+              的应用管理与计费、统计等商业运营功能，可以提供一套完整行业应用运营管理解决方案，助力用户快速实现应用商店的落地。
+            </div>
+            <button onClick={this.openVersionModal}>获取社区版 →</button>
+
+            <div className={styles.number}>
+              <label className={styles.currentNumber}>
+                <img
+                  className={styles.arrowLeft}
+                  src="/images/home/arrow-left.svg"
+                />
+                1
+              </label>
+              <label>
+                4
+                <img
+                  className={styles.arrowRight}
+                  src="/images/home/arrow-right.svg"
+                />
+              </label>
+            </div>
+          </div>
         </div>
-        <input placeholder="name@example.com" />
-        <button>下载</button>
-      </Modal>
+      </div>
     )
   }
 
@@ -72,84 +118,69 @@ export default class Home extends Component {
     )
   }
 
-  render() {
+  renderTopBanner() {
     return (
-      <div>
-        <div className={styles.topBanner}>
-          <Header maxTop={80} />
-          <div className={styles.wrapper}>
-            <div className={styles.title}>
-              <label className={styles.logo}>
-                <img src="/images/op-logo-blank.svg" />
-              </label>
-              <label className={styles.word}>多云应用管理平台</label>
-            </div>
-            <div className={styles.slogan}>
-              Run <label>any application</label> at any scale on{' '}
-              <label>any infrastructure</label>
-            </div>
-
-            <label className={styles.button}>获取社区版 →</label>
-
-            <img
-              src="/images/home/banner-top-top.svg"
-              className={styles.imgTop}
-            />
-            <img
-              src="/images/home/banner-top-bottom.svg"
-              className={styles.imgBottom}
-            />
+      <div className={styles.topBanner}>
+        <Header maxTop={80} />
+        <div className={styles.wrapper}>
+          <div className={styles.title}>
+            <label className={styles.logo}>
+              <img src="/images/op-logo-blank.svg" />
+            </label>
+            <label className={styles.word}>多云应用管理平台</label>
           </div>
+          <div className={styles.slogan}>
+            Run <label>any application</label> at any scale on{' '}
+            <label>any infrastructure</label>
+          </div>
+
+          <label onClick={this.openVersionModal} className={styles.button}>
+            获取社区版 →
+          </label>
+
           <img
-            src="/images/home/banner-top-right.svg"
-            className={styles.imgRight}
+            src="/images/home/banner-top-top.svg"
+            className={styles.imgTop}
+          />
+          <img
+            src="/images/home/banner-top-bottom.svg"
+            className={styles.imgBottom}
           />
         </div>
+        <img
+          src="/images/home/banner-top-right.svg"
+          className={styles.imgRight}
+        />
+      </div>
+    )
+  }
 
+  render() {
+    const { isDownloadOpen, isMailOpen, isMobile } = this.state
+
+    return (
+      <div>
+        {this.renderTopBanner()}
         {banners.map(banner => (
           <div className={styles.homeBanner} key={banner.name}>
             {this.renderBannerWord(banner)}
           </div>
         ))}
-
-        <div className={styles.sceneBanner}>
-          <div className={styles.sceneContent}>
-            <div className={styles.title}>应用场景</div>
-            <div className={styles.scene}>
-              <img
-                className={styles.currentImg}
-                src="/images/home/scene-1.svg"
-              />
-              <div className={styles.name}>快速构建行业应用商店 </div>
-              <div className={styles.description}>
-                在以应用为核心的行业云或集团云构建中，基于 OpenPitrix
-                的应用管理与计费、统计等商业运营功能，可以提供一套完整行业应用运营管理解决方案，助力用户快速实现应用商店的落地。
-              </div>
-              <button>获取社区版 →</button>
-
-              <div className={styles.number}>
-                <label className={styles.currentNumber}>
-                  <img
-                    className={styles.arrowLeft}
-                    src="/images/home/arrow-left.svg"
-                  />
-                  1
-                </label>
-                <label>
-                  4
-                  <img
-                    className={styles.arrowRight}
-                    src="/images/home/arrow-right.svg"
-                  />
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        {this.renderSceneBanner()}
         <Footer />
 
-        {/* {this.renderModal()}*/}
+        <DownloadModal
+          isOpen={isDownloadOpen}
+          isMobile={isMobile}
+          onClose={this.closeModal}
+          onSubmit={this.submitDownload}
+        />
+        <MailModal
+          isOpen={isMailOpen}
+          isMobile={isMobile}
+          onClose={this.closeModal}
+          onSubmit={this.closeModal}
+        />
       </div>
     )
   }

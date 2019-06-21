260ea418b1a8680bd2437ed5b027a3cc06710a04
Xi Wang <iwisunny@gmail.com>
2019-06-21 18:19:22 +0800

    style: Api doc page (#107)

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -17,7 +17,84 @@ export default class Api extends React.Component {
           <RedocStandalone
             options={{
               nativeScrollbars: true,
-              hideDownloadButton: true
+              hideDownloadButton: true,
+              theme: {
+                space: {
+                  unit: 4,
+                },
+                colors: {
+                  primary: {
+                    main: '#5628b4',
+                  },
+                  success: {
+                    main: '#29b565',
+                  },
+                  warning: {
+                    main: '#fabf3a',
+                  },
+                  error: {
+                    main: '#f04c41',
+                  },
+                  text: {
+                    primary: '#343945',
+                  },
+                  border: {
+                    dark: '#576075',
+                    light: '#8c96ad',
+                  },
+                  http: {
+                    get: '#29b565',
+                    post: '#0090ff',
+                    put: '#C660A0',
+                    options: '#d3ca12',
+                    patch: '#fabf3a',
+                    delete: '#f04c41',
+                    basic: '#a2a9be',
+                    link: '#31bbb6',
+                    head: '#c167e4',
+                  },
+                },
+                schema: {
+                  defaultDetailsWidth: '75%',
+                  labelsTextSize: '0.9em',
+                  nestingSpacing: '1em',
+                  nestedBackground: '#eff0f5',
+                  arrow: {
+                    size: '0.9em',
+                  },
+                },
+                typography: {
+                  fontSize: '12px',
+                  fontFamily: 'Roboto, sans-serif',
+                  smoothing: 'antialiased',
+                  optimizeSpeed: true,
+                  headings: {
+                    fontFamily: 'Roboto, sans-serif',
+                    fontWeight: '400',
+                    lineHeight: '1.168em',
+                  },
+                  code: {
+                    fontSize: '12px',
+                    fontFamily: 'SFMono-Regular,Consolas,Liberation Mono,Menlo,Courier,monospace',
+                    color: '#eff0f5',
+                    backgroundColor: '#474e5d',
+                    wrap: false,
+                  },
+                },
+                menu: {
+                  width: '280px',
+                  backgroundColor: '#f9fafb',
+                  textColor: '#576075',
+                  groupItems: {
+                    textTransform: 'lowercase',
+                  },
+                },
+                rightPanel: {
+                  backgroundColor: '#343945',
+                  width: '40%',
+                  textColor: '#eff0f5',
+                },
+              },
             }}
             spec={specJson}
           />
@@ -27,8 +104,8 @@ export default class Api extends React.Component {
   }
 }
 
-
 const Content=styled.div`
   position: relative;
-  top: 80px;
+  top: 72px;
+  background: #fff;
 `

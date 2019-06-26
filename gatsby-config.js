aa7325cb1a16c72eab52c9b6acbc681f47e34304
lizhi <lizhi@yunify.com>
2019-06-26 11:17:31 +0800

    feat: Support PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,26 +10,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     "gatsby-plugin-styled-components",
     {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          // {
-          //   resolve: "gatsby-remark-images",
-          //   options: {
-          //     maxWidth: 690
-          //   }
-          // },
-          {
-            resolve: "gatsby-remark-responsive-iframe"
-          },
-          "gatsby-remark-prismjs",
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-autolink-headers",
-          "gatsby-remark-format"
-        ]
-      }
-    },
-    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: `docs`,
@@ -62,5 +42,44 @@ module.exports = {
     // "gatsby-plugin-sharp",
     "gatsby-plugin-catch-links",
     "gatsby-transformer-json",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "OpenPitrix",
+        short_name: "openpitrix",
+        start_url: "/",
+        background_color: "#f9fafb",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "public/logo.svg", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+    'gatsby-plugin-offline',
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          // {
+          //   resolve: "gatsby-remark-images",
+          //   options: {
+          //     maxWidth: 690
+          //   }
+          // },
+          {
+            resolve: "gatsby-remark-responsive-iframe"
+          },
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-autolink-headers",
+          "gatsby-remark-format"
+        ]
+      }
+    },
   ],
 }
---
+++ gatsby.browser.js
@@ -0,0 +1,7 @@
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(`网站有版本更新，需要立即升级吗？`)
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}

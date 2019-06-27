bb486acf453836bbb643b0712ce8ba05ea3f5c9a
James <liiil825.2012@gmail.com>
2019-06-27 14:59:43 +0800

    feat: Support PWA (#114)

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,5 +62,23 @@ module.exports = {
     // "gatsby-plugin-sharp",
     "gatsby-plugin-catch-links",
     "gatsby-transformer-json",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "OpenPitrix",
+        short_name: "openpitrix",
+        start_url: "/",
+        background_color: "#f9fafb",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "./static/logo.svg", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ],
 }

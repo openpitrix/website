5f50f40f11496868183123a7c12e73076843cbe9
Xi Wang <iwisunny@gmail.com>
2019-08-28 22:41:45 +0800

    fix: Api page redoc inject style not working (#127)

--- src/templates/api.js
+++ src/templates/api.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import styled from 'styled-components';
 
 import Layout from 'layout';
@@ -6,27 +6,52 @@ import Header from 'components/Header';
 import Helmet from 'react-helmet';
 
 export default class Api extends React.Component {
-  componentDidMount() {
-    const {state}=this.props.pageContext;
-    // window.__redoc_state = JSON.stringify(state);
-
-    this.tmCheckRedoc=setInterval(()=> {
-      if(typeof Redoc !== 'undefined' && typeof Redoc.hydrate === 'function'){
-        Redoc.hydrate(state, document.getElementById('redoc'));
-        clearInterval(this.tmCheckRedoc);
-      }
-    }, 50);
+  constructor(props){
+    super(props);
+    this.state={
+      redocLoaded: false
+    }
   }
 
-  render() {
+  componentDidMount() {
+    this.tmCheckRedoc=setInterval(function(){
+      if(typeof Redoc !== 'undefined' && typeof Redoc.hydrate === 'function'){
+        clearInterval(this.tmCheckRedoc);
+        this.tmCheckRedoc=null;
+
+        this.setState({redocLoaded: true})
+      }
+    }.bind(this), 50);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(!prevState.redocLoaded && this.state.redocLoaded){
+      Redoc.hydrate(this.props.pageContext.state, document.getElementById('redoc'));
+    }
+  }
+
+  renderLoading(){
+    return (
+      <Loading>Loading..</Loading>
+    )
+  }
+
+  renderContent(){
     const {html, css}=this.props.pageContext;
+    const cssParts = css.match(/<style\s+([^>]+)>([^<]+)<\/style>/);
+    let cssBody;
+    if(cssParts && cssParts.length){
+      cssBody = cssParts[2]
+    }
 
     return (
-      <Layout>
+      <Fragment>
         <Helmet>
           <style>
             {
               `
+              ${typeof cssBody === 'string' && cssBody}
+              
               .redoc-json {
                 text-align: left !important;
               }
@@ -34,16 +59,33 @@ export default class Api extends React.Component {
             }
           </style>
         </Helmet>
+        <Content id="redoc" dangerouslySetInnerHTML={{ __html: html }}/>
+      </Fragment>
+    )
+  }
 
+  render() {
+    const {redocLoaded}=this.state;
+
+    return (
+      <Layout>
         <Header isBlankBg />
-        <Content id="redoc" dangerouslySetInnerHTML={{__html: css + html }} />
+        {!redocLoaded ? this.renderLoading() : this.renderContent()}
       </Layout>
     )
   }
 }
 
-const Content=styled.div`
+const Content = styled.div`
   position: relative;
   top: 72px;
   background: #fff;
 `
+
+const Loading = styled.div`
+  text-align: center;
+  position: relative;
+  top: 300px;
+  color: black;
+  font-size: 1.2rem;
+`;
